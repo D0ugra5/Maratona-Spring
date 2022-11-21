0bc878ed8abf4b5d6fc9d61d67c2473bc5c44e89
Douglas Sousa <douglasmb87@gmail.com>
2022-11-21 05:44:35 -0300

    feat:  organizing files and add AnimeController

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,8 +1,10 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +16,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AnimeController {
-    private DateUtil dateUtil;
-
-    @GetMapping(path = "list")
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
+    @GetMapping
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Dbz"), new Anime("Berserk"));
+        return animeService.listAll();
     }
 }
